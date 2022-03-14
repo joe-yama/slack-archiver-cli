4503bd4a91ce7c4b551e9c03b6408cb3314caaf4
joe-yama <s1r0mqme@gmail.com>
2022-03-14 18:25:04 +0900

    Fix to raise exception when token does not set.

--- slackarchiver/cli.py
+++ slackarchiver/cli.py
@@ -6,19 +6,11 @@ from typing import Dict, Any, List
 
 from slackarchiver.manage_channel import list_channels, archive_channels
 
-def cli_list_channels(
-    channel_prefix: str = "",
-    exclude_archived: bool = True,
-    include_private_channels: bool = False
-    ) -> int:
+def cli_list_channels() -> int:
     fire.Fire(list_channels)
     return 0
 
 
-def cli_archive_channels(
-    channel_prefix: str = "",
-    exclude_archived: bool = True,
-    include_private_channels: bool = False
-    ) -> int:
+def cli_archive_channels() -> int:
     fire.Fire(archive_channels)
     return 0
--- slackarchiver/settings.py
+++ slackarchiver/settings.py
@@ -2,5 +2,10 @@ import os
 
 import dotenv
 
+
 dotenv.load_dotenv(verbose=True)
+
+if "SLACK_BOT_TOKEN" not in os.environ:
+    raise KeyError("Environment variable `SLACK_BOT_TOKEN` was not found.")
+
 SLACK_BOT_TOKEN: str = os.environ.get("SLACK_BOT_TOKEN")
